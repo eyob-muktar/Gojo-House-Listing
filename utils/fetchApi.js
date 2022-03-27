75f8c97c476b5083f58839dff537d795163e1943
Eyob <eyobmuktar4@gmail.com>
2022-03-27 19:29:38 +0300

    refactor: extract api key to env variable

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,15 +1,6 @@
 import axios from "axios"
 
 export const baseUrl = 'https://bayut.p.rapidapi.com'
-// const options = {
-//     method: 'GET',
-//     url: 'https://bayut.p.rapidapi.com/agencies/list',
-//     params: {hitsPerPage: '25', page: '0', lang: 'en'},
-//     headers: {
-//       'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-//       'X-RapidAPI-Key': 'fe66b584a0msh37c69ea12c4cdb9p14e6efjsn75a9265a123e'
-//     }
-//   };
 
 export const fetchApi = async (url) => {
     try {
@@ -17,7 +8,7 @@ export const fetchApi = async (url) => {
         const { data } = await axios.get((url), {
             headers: {
                 'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-                'X-RapidAPI-Key': 'fe66b584a0msh37c69ea12c4cdb9p14e6efjsn75a9265a123e'
+                'X-RapidAPI-Key': process.env.RAPID_API_KEY
             }
         })
 
